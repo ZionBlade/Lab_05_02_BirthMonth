c3d717143512b46a66c19436c3dab746e66c66b9
ZionBlade <wernerct@mail.uc.edu>
2022-09-22 13:21:34 -0400

    finished commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int birthMonth = 45;
+
+        System.out.println("What is your birth month?");
+
+        if (birthMonth >= 1 && birthMonth <= 12)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else
+        {
+            System.out.println("You entered an incorrect month value: " + birthMonth);
+        }
     }
 }
